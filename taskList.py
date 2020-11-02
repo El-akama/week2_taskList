d957a258c47e20ad15a38febad7c79bd94979f8a
El-akama <akmatjanovel@gmail.com>
2020-11-02 14:51:39 +0600

    initial commit

---
+++ taskList.py
@@ -0,0 +1,88 @@
+# task1p1
+# f_name = ['Tilek', 'Kuba', 'Almaz', 'Bayaman', 'Ulugbek']
+# print(f_name[0])
+# print(f_name[1])
+# print(f_name[2])
+# print(f_name[3])
+# print(f_name[4])
+
+# task2p1
+# machines = ['Honda', 'BMW', 'Mersedes']
+# print(
+#     f'''I've the car {machines[0]}
+#     I like {machines[1]}
+#     Some people prefere a {machines[2]}.'''
+#     )
+
+# task3p1
+# spisok = ['pro1', 'pro2', 'pro3']
+# spisok.reverse()
+# print(spisok)
+
+# task4p1
+# nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+# numer = len(nums)
+# numero = numer // 2
+# nums1 = nums[:numero]
+# nums2 = nums[numero:]
+# print(nums2 + nums1)
+
+
+# Task1p2
+# f_name = ['Tilek', 'Kuba', 'Almaz', 'Bayaman', 'Ulugbek']
+# print(f"Hello, {f_name[0]}! Come for lunch today.")
+# print(f"Hello, {f_name[1]}! Come for lunch today.")
+# print(f"Hello, {f_name[2]}! Come for lunch today.")
+# print(f"Hello, {f_name[3]}! Come for lunch today.")
+# print(f"Hello, {f_name[4]}! Come for lunch today.")
+
+# Task2p2
+# g_name = ['Tilek', 'Kuba', 'Almaz', 'Bayaman', 'Ulugbek']
+# print(f"{g_name[3]} can’t come today.")
+# n_name = g_name.insert(3, 'Daniyar')
+# print(f"Hello, {g_name[0]}! Come for lunch today.")
+# print(f"Hello, {g_name[1]}! Come for lunch today.")
+# print(f"Hello, {g_name[2]}! Come for lunch today.")
+# print(f"Hello, {g_name[3]}! Come for lunch today.")
+# print(f"Hello, {g_name[4]}! Come for lunch today.")
+
+# Task3p2
+# g_name = ['Tilek', 'Kuba', 'Almaz', 'Bayaman', 'Ulugbek']
+# print("we wait more guests.")
+# guest1 = g_name.insert(0, "Rysbek")
+# guest2 = g_name.insert(3, "Daniyar")
+# guest3 = g_name.append("Kylym")
+# print(f"Hello, {g_name[0]}! Come for lunch today.")
+# print(f"Hello, {g_name[1]}! Come for lunch today.")
+# print(f"Hello, {g_name[2]}! Come for lunch today.")
+# print(f"Hello, {g_name[3]}! Come for lunch today.")
+# print(f"Hello, {g_name[4]}! Come for lunch today.")
+# print(f"Hello, {g_name[5]}! Come for lunch today.")
+# print(f"Hello, {g_name[6]}! Come for lunch today.")
+# print(f"Hello, {g_name[7]}! Come for lunch today.")
+
+# Task4p2
+# g_name = ['Tilek', 'Kuba', 'Almaz', 'Bayaman', 'Ulugbek']
+# print("Sorry, but we wait only two guests.")
+# l_g = g_name.pop()
+# print(f"{l_g}, I'm sorry about canceling the invitation.")
+# p_g = g_name.pop()
+# print(f"{p_g}, I'm sorry about canceling the invitation.")
+# s_g = g_name.pop()
+# print(f"{s_g}, I'm sorry about canceling the invitation.")
+# print(f"{g_name[0]}, the invitation remains valid.")
+# print(f"{g_name[1]}, the invitation remains valid.")
+# del g_name[:2]
+# print(g_name)
+
+# Task5p2
+# suitcase = []
+# f_case = suitcase.append("book")
+# f_case = suitcase.append("copybook")
+# f_case = suitcase.append("paper")
+# f_case = suitcase.append("pen")
+# f_case = suitcase.append("pencil")
+# print(suitcase)
+# suitcase.pop()
+# suitcase.insert(0, "notebook")
+# print(suitcase)
